0c120380e6cd339d4fb6fbca616c02b2b460005f
Luis-TI.dev.br <info@luis-ti.dev.br>
2026-02-23 19:13:06 -0300

    Correção do update.sh e keymap_config.yaml (#57)

--- keymap_img/update.sh
+++ keymap_img/update.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/usr/bin/nu
 
-echo 'Parsing ZMK keymap...'
-keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap > keymap.yaml
+echo '\n\nRemoving old ZMK keymap...'
+rm keymap.yaml
+echo '\n\nParsing ZMK keymap...'
+keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap | save keymap.yaml
 
 # echo '\n\nAdjusting keymap yaml...'
 # ./keymap_img_adjuster.py keymap.yaml
 
+echo '\n\nRemoving old keymap.svg...'
+rm keymap.svg
 echo '\n\nDrawing keymap...'
-keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s Base Nav Num Func Sym Adjust > keymap.svg
+keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml | save keymap.svg
