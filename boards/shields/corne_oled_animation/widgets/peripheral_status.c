df181010a663af5e19d6310b58540801f8279a76
Luis Diaz <info@luis-ti.dev.br>
2026-03-25 21:56:19 -0300

    Adição black lado direito

--- boards/shields/corne_oled_animation/widgets/peripheral_status.c
+++ boards/shields/corne_oled_animation/widgets/peripheral_status.c
@@ -35,6 +35,7 @@ LV_IMG_DECLARE(corro09);
 LV_IMG_DECLARE(corro10);
 LV_IMG_DECLARE(corro11);
 LV_IMG_DECLARE(corro12);
+LV_IMG_DECLARE(black);
 
 const lv_img_dsc_t *anim_imgs[] = {
     &corro01,
@@ -145,6 +146,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     //lv_img_set_src(art, &corro01);
 
     lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
+    lv_img_set_src(art, &black) //new line
     lv_obj_center(art);                                         //<--
     lv_animimg_set_src(art, (const void **) anim_imgs, 12);     //<--
     lv_animimg_set_duration(art, CONFIG_CUSTOM_ANIMATION_SPEED);//<--
